f702c8293bb76e19b92c500cdc22c5a71e3c756e
Nonato Sousa <nonato.sousa@antikytera.com>
2023-10-04 06:55:03 -0300

    * fix navigation view for iPadOS

--- Fructus/Screen/FruitDetailScreen.swift
+++ Fructus/Screen/FruitDetailScreen.swift
@@ -62,6 +62,7 @@ struct FruitDetailScreen: View {
                 }
             }
         }
+        .navigationViewStyle(.stack)
     }
 }
 
--- Fructus/Screen/FruitListScreen.swift
+++ Fructus/Screen/FruitListScreen.swift
@@ -34,6 +34,7 @@ struct FruitListScreen: View {
                     })
             )
         }
+        .navigationViewStyle(.stack)
     }
 }
 
